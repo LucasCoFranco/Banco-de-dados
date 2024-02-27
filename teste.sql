1e8971d2022a1005b61f92c4eb1f29b02908daea
Lucas Franco <136494921+LucasCoFranco@users.noreply.github.com>
2024-02-27 07:15:22 -0300

    Commit

--- teste.sql
+++ teste.sql
@@ -9,3 +9,8 @@ create table produto
  );
  insert into produto(codprod, dsprod, saldo, sldmin, prvenda, prcusto)
  values (101, 'Sabão em Pedra', 1470, 200, 1.87, 0.95);
+ 
+insert into produto values
+ (203, 'Frigideira', 0, 20, 18.50, 5),
+ (478, 'Guardanapo, 470, 100, 1.80, 0.20'),
+ (495, 'Alvejante Ipê', 3200, 3000, 2.76, 1.20);
