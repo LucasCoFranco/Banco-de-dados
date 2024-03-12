ebff5e08926b7ced295e89e35b85d8434f8b9256
Lucas Franco <136494921+LucasCoFranco@users.noreply.github.com>
2024-03-12 10:24:42 -0300

    Commit

--- Select.sql
+++ Select.sql
@@ -10,4 +10,31 @@ Mostrar uma linha especifica da tabela Produto
 Select dsprod, dsmedida, prvenda from Produto
 inner join UnidadeMedida ON
 UnidadeMedida.UN=produto.UN;
-Mostrar dados de duas tabelas
+Mostrar dados de duas tabelas com equivalencia de dados (un=un)(kg=kg)
+
+Select 
+         produto.dsprod,
+         UnidadeMedida.dsmedida,
+         produto.prvenda
+from Produto 
+left Join UnidadeMedida on Unidademedida.un = produto.un; 
+Mostrar dados de duas tabelas que incluem dados nulos
+(left é porque ele faz referencia ao dados a esquerda, porém ao contrario, ou seja a direita)
+
+Select 
+         produto.dsprod,
+         UnidadeMedida.dsmedida,
+         produto.prvenda
+from Produto 
+left Join UnidadeMedida on Unidademedida.un = produto.un 
+where dsmedida is null;
+Mostrar dados de dsmedida nulos
+
+delete from UnidadeMedida where un = 'UN';
+select * from produto where un = 'UN';
+deletar de UnidadeMedida os dados que possuem un
+
+
+update UnidadeMedida set un = 'K' where un = 'KG';
+select * from produto where un = 'KG';
+atualizar dados 
