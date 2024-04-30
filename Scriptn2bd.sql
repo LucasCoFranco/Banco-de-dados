a65dad3df041e049707af461cd1e9ea851807f7e
Lucas Franco <136494921+LucasCoFranco@users.noreply.github.com>
2024-04-30 08:42:08 -0300

    Create Scriptn2bd.sql

---
+++ Scriptn2bd.sql
@@ -0,0 +1,24 @@
+//idjogo, nome, genero, preço, quantidade disponivel, idtipmidia;
+1, God of War Ragnarok, ação-aventura, 299.90, 600, 1
+2, God of War Ragnarok, ação-aventura, 199.90, 100, 2
+3, Grand Theft Auto: San Andreas, ação-aventura, 65.00, 30, 2
+4, Red Dead Redemption, ação-aventura, 89.00, 50, 2
+5, Red Dead Redemption, ação-aventura, 99.00, 50, 1
+6, Red Dead Redemption 2, ação-aventura, 349.00, 200, 1
+7, Red Dead Redemption 2, ação-aventura, 299.00, 400, 2
+8, Horizon Forbidden West, ação-aventura, 99.00, 250, 2
+9, Horizon Forbidden West, ação-aventura, 149.00, 450, 1
+10, Grand Theft Auto V, ação-aventura, 99.00, 500, 2
+11, Grand Theft Auto V, ação-aventura, 149.00, 700, 1
+12, Naruto X Boruto: Ultamate Ninja Storm Connections, luta, 265.58, 520, 2
+13, Naruto X Boruto: Ultamate Ninja Storm Connections, luta, 299.00, 720, 1
+14, Hogwarts Legacy, RPG, 249.00, 450, 1
+15, Hogwarts Legacy, RPG, 229.00, 316, 2
+16, Mortal Kombat 1, luta, 249.00, 354, 1
+17, Mortal Kombat 1, luta, 209.00, 123, 2
+18, Street Fighter 6, luta, 249.00, 153, 1
+19, Street Fighter 6, luta, 249.00, 183, 2
+20,Gran Turismo 7, corrida, 259.00, 83, 1
+21,Gran Turismo 7, corrida, 249.00, 183, 2
+22,Ghost of Tsushima, ação-aventura, 199.00, 327, 1
+23,Ghost of Tsushima, ação-aventura, 199.00, 227, 2
