2bede28189653dce92740ba47959c83f6e2f2dfd
Lucas Franco <136494921+LucasCoFranco@users.noreply.github.com>
2024-05-14 08:34:34 -0300

    Update Scriptn2bd.sql

--- Scriptn2bd.sql
+++ Scriptn2bd.sql
@@ -110,31 +110,31 @@ VALUES
 
 --idjogo, nome, genero, preço, quantidade disponivel, idtipmidia;
 INSERT INTO Jogos VALUES
-(1, God of War Ragnarok, ação-aventura, 299.90, 600, 1),
-(2, God of War Ragnarok, ação-aventura, 199.90, 100, 2),
-(3, Grand Theft Auto: San Andreas, ação-aventura, 65.00, 30, 2),
-(4, Red Dead Redemption, ação-aventura, 89.00, 50, 1),
-(5, Red Dead Redemption, ação-aventura, 99.00, 50, 2),
-(6, Red Dead Redemption 2, ação-aventura, 349.00, 200, 1),
-(7, Red Dead Redemption 2, ação-aventura, 299.00, 400, 2),
-(8, Horizon Forbidden West, ação-aventura, 99.00, 250, 1),
-(9, Horizon Forbidden West, ação-aventura, 149.00, 450, 2),
-(10, Grand Theft Auto V, ação-aventura, 99.00, 500, 1),
-(11, Grand Theft Auto V, ação-aventura, 149.00, 700, 2),
-(12, Naruto X Boruto: Ultamate Ninja Storm Connections, luta, 265.58, 520, 1),
-(13, Naruto X Boruto: Ultamate Ninja Storm Connections, luta, 299.00, 720, 2),
-(14, Hogwarts Legacy, RPG, 249.00, 450, 1),
-(15, Hogwarts Legacy, RPG, 229.00, 316, 2),
-(16, Mortal Kombat 1, luta, 249.00, 354, 1),
-(17, Mortal Kombat 1, luta, 209.00, 123, 2),
-(18, Street Fighter 6, luta, 249.00, 153, 1),
-(19, Street Fighter 6, luta, 249.00, 183, 2),
-(20,Gran Turismo 7, corrida, 259.00, 83, 1),
-(21,Gran Turismo 7, corrida, 249.00, 183, 2),
-(22,Ghost of Tsushima, ação-aventura, 199.00, 327, 1),
-(23,Ghost of Tsushima, ação-aventura, 199.00, 227, 2);
-(24,Forza Horizon 5, corrida, 299.00, 330, 1);
-(25,Forza Horizon 5, corrida, 249.00, 500, 2);
+(1, 'God of War Ragnarok', ação-aventura, 299.90, 600, 1),
+(2, 'God of War Ragnarok', ação-aventura, 199.90, 100, 2),
+(3, 'Grand Theft Auto: San Andreas', ação-aventura, 65.00, 30, 2),
+(4, 'Red Dead Redemption', ação-aventura, 89.00, 50, 1),
+(5, 'Red Dead Redemption', ação-aventura, 99.00, 50, 2),
+(6, 'Red Dead Redemption 2', ação-aventura, 349.00, 200, 1),
+(7, 'Red Dead Redemption 2', ação-aventura, 299.00, 400, 2),
+(8, 'Horizon Forbidden West', ação-aventura, 99.00, 250, 1),
+(9, 'Horizon Forbidden West', ação-aventura, 149.00, 450, 2),
+(10, 'Grand Theft Auto V', ação-aventura, 99.00, 500, 1),
+(11, 'Grand Theft Auto V', ação-aventura, 149.00, 700, 2),
+(12, 'Naruto X Boruto: Ultamate Ninja Storm Connections', luta, 265.58, 520, 1),
+(13, 'Naruto X Boruto: Ultamate Ninja Storm Connections', luta, 299.00, 720, 2),
+(14, 'Hogwarts Legacy', RPG, 249.00, 450, 1),
+(15, 'Hogwarts Legacy', RPG, 229.00, 316, 2),
+(16, 'Mortal Kombat 1', luta, 249.00, 354, 1),
+(17, 'Mortal Kombat 1', luta, 209.00, 123, 2),
+(18, 'Street Fighter 6', luta, 249.00, 153, 1),
+(19, 'Street Fighter 6', luta, 249.00, 183, 2),
+(20, 'Gran Turismo 7', corrida, 259.00, 83, 1),
+(21, 'Gran Turismo 7', corrida, 249.00, 183, 2),
+(22, 'Ghost of Tsushima', ação-aventura, 199.00, 327, 1),
+(23, 'Ghost of Tsushima', ação-aventura, 199.00, 227, 2);
+(24, 'Forza Horizon 5', corrida, 299.00, 330, 1);
+(25, 'Forza Horizon 5', corrida, 249.00, 500, 2);
 
 INSERT INTO Plataformas values
 --id, nomeplat
